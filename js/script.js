f5de8e70a2118ad9fe301e3545bc658f9a989459
Caio César Brito <caio.25.brito@gmail.com>
2022-06-16 13:45:49 -0300

    Adição das setinhas de controle na versão tablet e desktop

--- js/script.js
+++ js/script.js
@@ -2,7 +2,6 @@ document.addEventListener( 'DOMContentLoaded', function () {
   const splide = new Splide('#splide1', {
     type: 'loop',
     perPage: 1,
-    arrows: false,
     autoplay: false,
     interval: 3000,
   });
@@ -11,7 +10,6 @@ document.addEventListener( 'DOMContentLoaded', function () {
   const splide2 = new Splide('#splide2', {
     type: 'loop',
     perPage: 1,
-    arrows: false,
     autoplay: false,
     interval: 3000,
   });
